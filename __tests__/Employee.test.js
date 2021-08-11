750e98e9d90f98d57a193981120283e38b13ff30
mjh1985codeman <Michael.Hodges1985@Yahoo.com>
2021-08-11 18:56:25 -0500

    began testing work

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,9 +1,53 @@
-const Employee = require("../lib/Employee.js");
+const Employee = require("../lib/Employee");
 
+//test to see if class returns object
 test("creates a employee object", () => {
-  const employee = new Employee("Michael");
+  const emp = new Employee();
+  expect(typeof emp).toBe("object");
+});
 
-  expect(employee.name).toBe("Michael");
-  expect(employee.id).toBe("123");
-  expect(employee.Email).toBe("me@email.com");
+// tests for arguments
+
+test("Grabs Name from Argument", () => {
+  const n = "Michael";
+  const emp = new Employee(n);
+  expect(emp.name).toBe(n);
+});
+
+test("Grabs Id from Argument", () => {
+  const id = "123";
+  const emp = new Employee("Michael", id);
+  expect(emp.id).toBe(id);
+});
+
+test("Grabs Email from Argument", () => {
+  const email = "test@email.com";
+  const emp = new Employee("Michael", "123", email);
+  expect(emp.email).toBe(email);
+});
+
+// tests for method
+
+test("Grabs Name from getName()", () => {
+  const n = "Michael";
+  const emp = new Employee(n);
+  expect(emp.getName()).toBe(n);
+});
+
+test("Grabs Id from getId()", () => {
+  const id = "123";
+  const emp = new Employee("Michael", id);
+  expect(emp.getId()).toBe(id);
+});
+
+test("Grabs Email from getEmail()", () => {
+  const email = "test@email.com";
+  const emp = new Employee("Michael", "123", email);
+  expect(emp.getEmail()).toBe(email);
+});
+
+test("Grabs Email from getRole()", () => {
+  const role = "Employee";
+  const emp = new Employee("Michael", "123", "test@email.com");
+  expect(emp.getRole()).toBe(role);
 });
