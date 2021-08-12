c7108bb62a12806a5f44ea86ac478bcf38431a86
mjh1985codeman <Michael.Hodges1985@Yahoo.com>
2021-08-11 21:10:30 -0500

    Engineer Test passed

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,7 +1,7 @@
 const Engineer = require("../lib/Engineer.js");
 
 //test to see if class returns object
-test("creates a intern object", () => {
+test("creates a Engineer object", () => {
   const eng = new Engineer();
   expect(typeof eng).toBe("object");
 });
@@ -9,6 +9,6 @@ test("creates a intern object", () => {
 //test for arguments
 test("Grabs GitHub from Argument", () => {
   const gh = "mjhCodeMan1985";
-  const eng = new Engineer(gh);
+  const eng = new Engineer("Michael", "123", "test@email.com", gh);
   expect(eng.gitHub).toBe(gh);
 });
